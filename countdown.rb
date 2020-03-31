952307e20f02a79772b21c1ea0ad8e3d59501fde
DAZUN XING <dazun.xing@gmail.com>
2020-03-31 23:24:52 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,23 +1,25 @@
 #write your code here
 
-def countdown(int)
-  while int>0 
-    puts"#{int} SECOND(S)!"
-    int-=1 
-  end
-  return "HAPPY NEW YEAR!"
-end
+# def countdown(int)
+#   while int>0 
+#     puts"#{int} SECOND(S)!"
+#     int-=1 
+#   end
+#   return "HAPPY NEW YEAR!"
+# end
 
 
-def countdown_with_sleep (k)
-  while int>0 
-    sleep (1.5)
-    puts"#{k} SECOND(S)!"
-    k-=1 
-    # sleep (1.5)
-  end
-  return "HAPPY NEW YEAR!"
-end
+# def countdown_with_sleep (k)
+#   while int>0 
+#     sleep (1.5)
+#     puts"#{k} SECOND(S)!"
+#     k-=1 
+#     # sleep (1.5)
+#   end
+#   return "HAPPY NEW YEAR!"
+# end
   
-  
+  puts "Hello world! Greetings from Includehelp.com"
+sleep 5
+puts "bye"
   
