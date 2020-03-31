69006213f2bbd34d4ef78cce102072ef5bc2c84b
DAZUN XING <dazun.xing@gmail.com>
2020-03-31 23:29:19 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,23 @@
 #write your code here
 
-def countdown
+def countdown(int)
+  while int>0 
+    puts"#{int} SECOND(S)!"
+    int-=1 
+  end
+  return "HAPPY NEW YEAR!"
 end
+
+
+def countdown_with_sleep (k)
+  while k>0 
+    sleep (1.5)
+    puts"#{k} SECOND(S)!"
+    k-=1 
+    sleep 1
+  end
+  return "HAPPY NEW YEAR!"
+end
+  
+
+  
